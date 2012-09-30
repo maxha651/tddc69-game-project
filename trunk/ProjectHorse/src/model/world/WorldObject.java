4c251df115094697b2017f034728b8983b9a1d0a
frepr183@student.liu.se <frepr183@student.liu.se@f1a298f0-f6b0-0cc4-c04d-752dd446bdd0>
2012-09-30 18:36:39 +0000

    Rotation on bounds fixed! :D loooks awezome

--- trunk/ProjectHorse/src/model/world/WorldObject.java
+++ trunk/ProjectHorse/src/model/world/WorldObject.java
@@ -21,7 +21,24 @@ public abstract class WorldObject extends AbstractGameObject implements Boundabl
     protected ZoneCoordinate zoneCoordinate;
     protected double rotationAngle;
     protected double width = 15, height = 1;
+    protected boolean alive = true;
+    protected boolean remove = false;
 
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public boolean isRemove() {
+        return remove;
+    }
+
+    public void setRemove(boolean remove) {
+        this.remove = remove;
+    }
 
     public double getWidth() {
         return width;
